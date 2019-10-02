37c36ecba64198ae89e0ec9c97b2d8b309a6e4d5
Bogdan <55501437+anddxr@users.noreply.github.com>
2019-10-02 19:00:33 +0300

    notdone

--- src/index.js
+++ src/index.js
@@ -7,49 +7,57 @@ Vue.use(VueAxios,axios)
 new Vue({
     el: '#zad',
     data: {
-      regions: [],
-      city:[],
-      gym:[],
-      selectedRegion:'',
-      selectedCity:'',
+      students: [],
+      search:'',
+      usd:0,
+      changeId:'',
+      money:'',
+      curr: [],
+      name:'',
+      group:'',
+      isDonePr:false,
     },
-
     mounted: function(){
-     
-      Vue.axios.post("https://api.novaposhta.ua/v2.0/json/",{
-          "apiKey": "a7e789fde9d8eb6561e499f1adc0e6d3",
-          "modelName": "Address",
-          "calledMethod": "getAreas",
-          "methodProperties": {}
+      Vue.axios.get("http://46.101.212.195:3000/students").then((response) =>{
+        console.log(response.data)
+        this.students = response.data;
       })
-      .then((response) => {
-        this.regions = response.data.data;
-        this.mount=!this.mount;
-      }),
-      Vue.axios.post("https://api.novaposhta.ua/v2.0/json/",{
-        "apiKey": "a7e789fde9d8eb6561e499f1adc0e6d3",
-        "modelName": "Address",
-        "calledMethod": "getCities",
-        "methodProperties": {}
-    })
-    .then((response) => {
-      this.city = response.data.data;
-      this.mount=!this.mount;
-    })
-    
+
+      Vue.axios.get("http://apilayer.net/api/live?access_key=e5f9f31b0eb758ba092f2089065089e4&currencies=UAH,EUR&source=USD&format=1").then((response) =>{
+        console.log(response.data)
+        this.curr = response.data;
+      })
     },
     methods: {
-      findWare: function(){
-        Vue.axios.post("https://api.novaposhta.ua/v2.0/json/",{
-          "apiKey": "a7e789fde9d8eb6561e499f1adc0e6d3",
-          "modelName": "Address",
-          "calledMethod": "getWarehouses",
-          "methodProperties": {"CityName":this.selectedCity}
+      clickme: function(id){
+        alert("Ok");
+        this.students = this.students.filter((element) => { 
+          return element.id !== id;
+        });
+      },
+      AddNewStud: function(){
+        Vue.axios.post("http://46.101.212.195:3000/students",{
+          name: this.name,
+          group: this.group,
+          isDonePr: false
+        }).then((responce) => {
+          console.log(responce.data)
+        })
+        this.GetList()
+      },
+      DlStudentById: function(id){
+        console.log(id)
+        Vue.axios.delete('http://46.101.212.195:3000/students/'+id)
+        this.GetList()
+      },
+      GetList: function(){
+        Vue.axios.get("http://46.101.212.195:3000/students").then((response) =>{
+        console.log(response.data)
+        this.students = response.data;
       })
-      .then((response) => {
-        this.gym = response.data.data;
-        this.mount=!this.mount;
-      })
+      },
+      UpdtById: function(){
+
       }
     },
 });
